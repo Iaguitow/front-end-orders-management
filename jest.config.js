61453d0863f80610aaa388cdc70481b36cc91019
Iago Silva Vieira <vieiras.igs@gmail.com>
2025-07-04 18:45:45 +0100

    Next CI test

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,20 @@
-module.exports = {
+/** @type {import('jest').Config} */
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '^@/components/(.*)$': '<rootDir>/components/$1',
+    '^@/pages/(.*)$': '<rootDir>/pages/$1',
+  },
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
 };
+
+module.exports = createJestConfig(customJestConfig);
