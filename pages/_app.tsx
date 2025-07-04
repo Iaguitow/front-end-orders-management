af605e1db48ef2792396ba275492b9746abdef51
Iago Silva Vieira <vieiras.igs@gmail.com>
2025-07-04 18:19:55 +0100

    Testing CI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import { Provider } from 'react-redux';
 import { store } from '../store';
 import 'antd/dist/reset.css';
-import '../styles/globals.css';
+import '../src/app/globals.css';
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
